d56aead8d411f35eeea07dfaa200e0d9b28aa8a3
Ross Anderson <ross.anderson@ualberta.ca>
2016-02-17 11:50:52 -0700

    builtin: if, also fix paren typos

--- 1363708.lisp
+++ 1363708.lisp
@@ -4,24 +4,26 @@
       (t (let ( (f (car E))  (arg (cdr E)) )
 	   (cond 
             ; handle built-in functions
-            ((eq f 'if) nil) ; todo if
+            ((eq f 'if) (if (fl-interp (car arg) P)
+                            (fl-interp (cadr arg) P)
+                            (fl-interp (caddr arg) P)))
             ((eq f 'null) (null (fl-interp (car arg) P)))
             ((eq f 'atom) (atom (fl-interp (car arg) P)))
-            ((eq f 'eq) (eq (fl-interp (car arg) P) fl-interp (cadr arg) P))
+            ((eq f 'eq) (eq (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
             ((eq f 'first)  (car (fl-interp (car arg) P)))
             ((eq f 'rest) (cdr (fl-interp (car arg) P)))
-            ((eq f 'cons) (cons (fl-interp (car arg) P) fl-interp (cadr arg) P))
-            ((eq f 'equal) (equal (fl-interp (car arg) P) fl-interp (cadr arg) P))
+            ((eq f 'cons) (cons (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+            ((eq f 'equal) (equal (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
             ((eq f 'number) (numberp (fl-interp (car arg) P)))
-            ((eq f '+) (+ (fl-interp (car arg) P) (fl-interp (cadr arg) P))
-            ((eq f '-) (- (fl-interp (car arg) P) (fl-interp (cadr arg) P))
-            ((eq f '*) (* (fl-interp (car arg) P) fl-interp (cadr arg) P))
-            ((eq f '>) (> (fl-interp (car arg) P) fl-interp (cadr arg) P))
-            ((eq f '<) (< (fl-interp (car arg) P) fl-interp (cadr arg) P))
-            ((eq f '=) (= (fl-interp (car arg) P) fl-interp (cadr arg) P))
+            ((eq f '+) (+ (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+            ((eq f '-) (- (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+            ((eq f '*) (* (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+            ((eq f '>) (> (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+            ((eq f '<) (< (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+            ((eq f '=) (= (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
             ((eq f 'and) nil) ; todo and
             ((eq f 'or) nil) ; todo or
-            ((eq f 'not) (not (fl-interp (car arg) P))
+            ((eq f 'not) (not (fl-interp (car arg) P)))
             
 	      ; if f is a user-defined function,
             ; then evaluate the arguments 
