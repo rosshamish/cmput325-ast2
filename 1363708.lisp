0305970260f860a41e24c2a4ba47cb403bc3ae04
Ross Anderson <ross.anderson@ualberta.ca>
2016-02-17 11:08:28 -0700

    initial commit. skeleton, builtin todos

---
+++ 1363708.lisp
@@ -0,0 +1,34 @@
+(defun fl-interp (E P)
+  (cond 
+	((atom E) E)
+    (t (let ( (f (car E))  (arg (cdr E)) )
+	   (cond 
+            ; handle built-in functions
+            ((eq f 'if) nil) ; todo if
+            ((eq f 'null) nil) ; todo null
+            ((eq f 'atom) nil) ; todo atom
+            ((eq f 'eq) nil) ; todo eq
+            ((eq f 'first)  (car (fl-interp (car arg) P)))
+            ((eq f 'rest) nil) ; todo rest
+            ((eq f 'cons) nil) ; todo cons
+            ((eq f 'equal) nil) ; todo equal
+            ((eq f 'number) nil) ; todo number
+            ((eq f '+) nil) ; todo +
+            ((eq f '-) nil) ; todo -
+            ((eq f '*) nil) ; todo *
+            ((eq f '>) nil) ; todo >
+            ((eq f '<) nil) ; todo <
+            ((eq f '=) nil) ; todo =
+            ((eq f 'and) nil) ; todo and
+            ((eq f 'or) nil) ; todo or
+            ((eq f 'not) nil) ; todo not
+            
+	        ; if f is a user-defined function,
+            ; then evaluate the arguments 
+            ; and apply f to the evaluated arguments 
+            ; (applicative order reduction) 
+            ; todo
+
+            ; otherwise f is undefined; in this case,
+            ; E is returned as if it is quoted in lisp
+            (t E))))))
